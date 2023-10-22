d5387af455d3027745fd312a5532c6770220faaa
WorkingPranav <108082438+WorkingPranav@users.noreply.github.com>
2023-10-22 20:20:41 +0530

    Update __init__.py

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -1,5 +1,12 @@
 import os, sys, logging
 from pyrogram import Client 
+from aiohttp import web
+from .route import routes
+
+async def web_server():
+    web_app = web.Application(client_max_size=30000000)
+    web_app.add_routes(routes)
+    return web_app 
 
 if os.path.exists('error.log'):
     os.remove('error.log')
